a55e59e05f90c107ac6a208d59527a1744204c07
Muhammad Syah Zannuar S <zannuarmailhere@gmail.com>
2021-06-11 21:46:16 +0700

    Add files via upload

---
+++ Makefile
@@ -0,0 +1,7 @@
+CONTIKI_PROJECT = demo
+all: $(CONTIKI_PROJECT)
+
+PROJECT_SOURCEFILES += mytemp.c
+
+CONTIKI = ../../
+include $(CONTIKI)/Makefile.include
---
+++ mytemp.h
@@ -0,0 +1,10 @@
+#ifndef MYTEMP_H
+#define MYTEMP_H
+struct Sensor {
+ char name[15];
+ float value;
+};
+struct Sensor read_temperature();
+struct Sensor read_humidity();
+struct Sensor read_pressure();
+#endif
